eb3b2c1182f23615ebcfb77c2aa66c26596d2eeb
Sid Patel <sypatel18@gmail.com>
2020-03-04 11:28:32 -0500

    Create AppleandOrange.cpp

---
+++ AppleandOrange.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the countApplesAndOranges function below.
+void countApplesAndOranges(int s, int t, int a, int b, vector<int> apples, vector<int> oranges) {
+    int applecounter = 0;
+    int orangecounter = 0;
+    for (unsigned int i = 0; i < apples.size(); i++){
+        apples[i] = apples[i] + a;
+    }
+    for (unsigned int j = 0; j < oranges.size(); j++){
+        oranges[j] = oranges[j] + b;
+    }
+    for (unsigned int i = 0; i < apples.size(); i++){
+        if (apples[i]>=s && apples[i]<=t){
+            applecounter++;
+        }
+    }
+    for (unsigned int j = 0; j < oranges.size(); j++){
+        if (oranges[j]>=s && oranges[j]<=t){
+            orangecounter++;
+        }
+    }
+    cout << applecounter << "\n";
+    cout << orangecounter;
+
+
+}
